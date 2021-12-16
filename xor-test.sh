30209e915a33e3cbb05b8579c68885c8fd89d389
Daniel Kerrigan <dkerriga@nd.edu>
2021-12-15 23:31:47 -0500

    update xor test, remove unnecessary files

--- xor-test.sh
+++ xor-test.sh
@@ -3,3 +3,9 @@
 ./run ubuntu.iso -r 512 5505348
 ./run ubuntu.iso -r 1024 2752674
 ./run ubuntu.iso -r 2048 1376337
+
+./fast ubuntu.iso
+
+./run ubuntu.iso -f 1024 4
+./run ubuntu.iso -f 2048 16
+./run ubuntu.iso -f 1048576 32
